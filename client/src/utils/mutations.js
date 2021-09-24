9d68545c9fed057938dabd1b2b708bc388f5b32f
Duncan Byrne <duncanbyrne@Duncans-MacBook-Pro.local>
2021-09-24 14:24:51 -0400

    simplified the queries and mutations in the utils folders on the client side

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,23 +1,11 @@
 import { gql } from '@apollo/client';
 
-export const CREATE_PROFILE = gql`
-  mutation createProfile($profile: ProfileData) {
-    createProfile(profile: $profile) {
-      success
-      profile {
-        id
-      }
-    }
-  }
-`;
-
-export const ADD_ARTIST = gql`
-  mutation addArtist($artist: ArtistData!) {
-    createArtist(artist: $artist) {
-      success
-      artist {
-        id
-      }
+export const ADD_PROFILE = gql`
+  mutation addProfile($name: String!) {
+    addProfile(name: $name) {
+      _id
+      name
+      skills
     }
   }
 `;

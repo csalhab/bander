791f1362dd55dcb147dddcd604e0cb1c17e89ffe
Duncan Byrne <duncanbyrne@Duncans-MacBook-Pro.local>
2021-09-24 15:33:55 -0400

    fixed the problems in the pull request

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-steph-routers
+
 import React from "react";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import { BrowserRouter as Router, Route } from "react-router-dom";
@@ -8,6 +8,8 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Search from "./pages/Search";
 import UserProfile from "./pages/UserProfile";
+import Header from "./components/Header";
+
 
 const client = new ApolloClient({
   uri: "/graphql",
@@ -26,48 +28,18 @@ function App() {
             </Route>
             {/* Create a route to display a User's comments based on its `UserID` provided in the URL */}
             <Route exact path="/UserProfile/:thoughtId">
-              <SingleThought />
+              <UserProfile />
             </Route>
             <Route exact path="/login">
               <Login />
             </Route>
           </div>
-          <Footer />
+          
         </div>
       </Router>
-    </ApolloProvider>
+    </ApolloProvider>)
 
-import logo from "./logo.svg";
-import "./App.css";
-import LoginButton from "./components/LoginButton";
-import SearchBar from "./components/SearchBar";
-import UserCard from "./components/UserCards";
-import SearchButton from "./components/SearchButton";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <LoginButton>Login</LoginButton>
-        <SearchBar>SearchBar</SearchBar>
-        <UserCard></UserCard>
-        <SearchButton></SearchButton>
-      </header>
-    </div>
-main
-  );
 }
 
 export default App;

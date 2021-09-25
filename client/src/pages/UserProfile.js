a08239346d4fc71e5a5faacf34809b7d43efbef2
Stephanie Blanchard <sblanchard2432@gmail.com>
2021-09-25 16:22:26 -0400

    added place holder in profile

--- client/src/pages/UserProfile.js
+++ client/src/pages/UserProfile.js
@@ -4,6 +4,12 @@ const UserProfile = () => {
   return (
     <div className="container">
       <p>This is the user profile page.</p>
+      <img src="" alt="Profile" />
+
+      <h2>name</h2>
+      <p>contact info/email/fax</p>
+      <p>description</p>
+      <p>bio</p>
     </div>
   );
 };

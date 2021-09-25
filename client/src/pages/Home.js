3f74f65c231afe4a2b9ade398f4d21984ce121a1
LowGcifer <LowGcifer@gmail.com>
2021-09-25 15:49:45 -0400

    made mockups of the signup, login, added search bar, added a signup route.

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,16 @@
 import React from "react";
 import SignupButton from "../components/SignupButton";
 import LoginButton from "../components/LoginButton";
+import SearchBar from "../components/SearchBar";
+import SearchButton from "../components/SearchButton";
 
 const Home = () => {
   return (
     <div className="container">
       <LoginButton></LoginButton>
       <SignupButton></SignupButton>
+      <SearchBar></SearchBar>
+      <SearchButton></SearchButton>
     </div>
   );
 };

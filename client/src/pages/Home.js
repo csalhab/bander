f3f5c32083565cf556fbbb3c619f9bbd1dfed752
LowGcifer <LowGcifer@gmail.com>
2021-09-27 11:32:47 -0400

    footer tomfoolery

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,6 +5,7 @@ import SearchBar from "../components/SearchBar";
 import SearchButton from "../components/SearchButton";
 import LogoutButton from "../components/LogoutButton";
 import Auth from "../utils/auth";
+import Footer from "../components/Footer";
 
 const Home = () => {
   if (Auth.loggedIn()) {
@@ -16,6 +17,7 @@ const Home = () => {
           Input the instrument or role you are looking for in the search bar!
           Click the button when you're ready to go looking for bandmates!
         </div>
+        {/* <Footer></Footer> */}
       </div>
     );
   } else {

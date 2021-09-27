f4c720f239851ec934727a4f3418fbe31ddb0107
Claudia Correa Salhab <claudiacorreasalhab@gmail.com>
2021-09-27 15:51:58 -0400

    fixed nav for signup and login too

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -90,16 +90,67 @@ function Header({ children }) {
                 </form>
               </div>
             </nav>
-            <div style={styles.buttonContainer} className="container">
-              <LogoutButton></LogoutButton>
-            </div>
           </>
         ) : (
+          
           <>
-            <div className="container">
-              <LoginButton></LoginButton>
-              <SignupButton></SignupButton>
-            </div>
+            
+            <nav class="navbar navbar-expand-lg navbar-light bg-light navbar-style">
+              <a class="navbar-brand" href="#">
+                Bander
+              </a>
+              <button
+                class="navbar-toggler"
+                type="button"
+                data-toggle="collapse"
+                data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <span class="navbar-toggler-icon"></span>
+              </button>
+
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                  <li class="nav-item active">
+                    <a class="nav-link" href="/">
+                      Home <span class="sr-only">(current)</span>
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="/favorites">
+                      Favorites
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="/profile">
+                      Profile
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <Nav.Link href="/login">Login</Nav.Link>
+                  </li>
+                  <li class="nav-item">
+                    <Nav.Link href="/signup">Sign Up</Nav.Link>
+                  </li>
+                </ul>
+                <form class="form-inline my-2 my-lg-0">
+                  <input
+                    class="form-control mr-sm-2"
+                    type="search"
+                    placeholder="Search"
+                    aria-label="Search"
+                  ></input>
+                  <button
+                    class="btn btn-outline-success my-2 my-sm-0"
+                    type="submit"
+                  >
+                    Search
+                  </button>
+                </form>
+              </div>
+            </nav>
           </>
         )}
         ;
